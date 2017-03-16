6a4d71cef22c339af6dd3b446a5e491bb610add8
jhaggqvist <jesper.haggqvist@students.stamford.edu>
2017-03-16 23:18:52 +0700

    first and most likely only commit

---
+++ Start_Stop_relay.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO #imports GPIO libraries
+from time import sleep  #imports sleep function
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+print "Relay is turned on" 
+GPIO.setup(11, GPIO.OUT) #sets gpio pin 11 
+GPIO.output(11, 1) # sets GPIO 11 as HIGH (on)
+sleep(5) #pauses the script for 5 seconds.
+print "Relay is turned off"
+GPIO.output(11, 0) #sets  GPIO 11 as LOW (off)
+GPIO.cleanup() #use this to  restore gpio default values
