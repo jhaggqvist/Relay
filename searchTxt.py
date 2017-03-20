966c27dd17ae0ab8fcc7abeb6f118e2d1d0956d7
jhaggqvist <jesper.haggqvist@students.stamford.edu>
2017-03-20 15:34:25 +0700

    new file:   searchTxt.py

---
+++ searchTxt.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+script, filename, term = argv
+
+f = open(filename)
+
+def start_program():
+    line_number = 0
+
+    for word in f.read().split(): 
+        line_number += 1
+        if word == term:    
+            print "Searching for %s in %s filename" % (word, filename)
+            print "%s was successfully found in %s at line number %s" % (word, filename, line_number)
+            
+start_program()
+
